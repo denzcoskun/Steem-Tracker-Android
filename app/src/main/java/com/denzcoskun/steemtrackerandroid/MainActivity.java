f182de4b51b689500bfd6f40e328de44984e7eee
denzcoskun <Deneme57>
2018-02-18 01:50:18 +0300

    Project has been created.

---
+++ app/src/main/java/com/denzcoskun/steemtrackerandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.denzcoskun.steemtrackerandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
