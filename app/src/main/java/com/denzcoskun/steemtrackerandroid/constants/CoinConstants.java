2b22911eb36b17426f7944513da6ed4f4e7e97d2
Deniz Coskun <denzcoskun@hotmail.com>
2018-02-22 00:32:14 +0300

    Constants have been added.

---
+++ app/src/main/java/com/denzcoskun/steemtrackerandroid/constants/CoinConstants.java
@@ -0,0 +1,12 @@
+package com.denzcoskun.steemtrackerandroid.constants;
+
+/**
+ * Created by Denx on 21.02.2018.
+ */
+
+public class CoinConstants {
+
+    public static String BITCOIN = "bitcoin";
+    public static String STEEM = "steem";
+    public static String STEEMDOLLAR = "steem-dollars";
+}
---
+++ app/src/main/java/com/denzcoskun/steemtrackerandroid/constants/CurrencyConstants.java
@@ -0,0 +1,14 @@
+package com.denzcoskun.steemtrackerandroid.constants;
+
+/**
+ * Created by Denx on 21.02.2018.
+ */
+
+public class CurrencyConstants {
+
+    public static String DOLLAR = "USD";
+    public static String EURO = "EUR";
+    public static String LIRA = "TRY";
+    public static String RUBLE = "RUB";
+
+}
